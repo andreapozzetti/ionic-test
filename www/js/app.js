de18cf846c50209dcbb71ff02eda8f545aa17e5d
andrepozz <andrepozz@inwind.it>
2015-05-29 17:15:02 +0200

    test

--- www/js/app.js
+++ www/js/app.js
@@ -8,16 +8,7 @@ window.ionic.Platform.ready(function() {
 angular.module('starter', ['ionic', 'gettext', 'starter.controllers', 'starter.services'])
 .run(function($ionicPlatform, $rootScope, gettextCatalog) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-
+ 
     /* LANGUAGE */
 
     if(typeof navigator.globalization !== "undefined") {
